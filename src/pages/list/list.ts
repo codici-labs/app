44d60c33504825aa54905f3f84b4f3c72cef1f06
mpandolfelli <martinpandolfelli@gmail.com>
2017-10-07 13:54:34 -0300

    primer commit

---
+++ src/pages/list/list.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailsPage } from '../item-details/item-details';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+
+    this.http.get('http://localhost/card-project/api/getProducts').map(res => res.json()).subscribe(data => {
+        
+        this.items = data;
+    });
+
+
+    /*this.items = [];
+    for(let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }*/
+  }
+
+  itemTapped(event, item) {
+    this.navCtrl.push(ItemDetailsPage, {
+      item: item
+    });
+  }
+}
