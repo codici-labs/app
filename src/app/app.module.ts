9103806fe1b34e47a9d40e87e7290cadadf5f89f
mpandolfelli <martinpandolfelli@gmail.com>
2017-10-07 16:06:50 -0300

    agrego camara

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HttpModule } from '@angular/http';
+import { Camera } from '@ionic-native/camera';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -32,6 +34,7 @@ import { HttpModule } from '@angular/http';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -5,6 +5,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { ItemDetailsPage } from '../item-details/item-details';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
@@ -13,12 +15,12 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, private camera: Camera) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
 
-    this.http.get('http://localhost/card-project/api/getProducts').map(res => res.json()).subscribe(data => {
+    this.http.get('http://codicilabs.com/proyectos/card/index.php/api/getProducts').map(res => res.json()).subscribe(data => {
         
         this.items = data;
     });
@@ -39,4 +41,23 @@ export class ListPage {
       item: item
     });
   }
+
+  showCamera(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     console.log(err);
+    });
+  }
+
+
 }
