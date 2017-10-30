e29cd2c50a76b1174597b0b8b01fb6c1270ca12c
mpandolfelli <martinpandolfelli@gmail.com>
2017-10-30 18:06:15 -0300

    modal con listado de productos, falta el buscar

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 /**
  * Generated class for the ModalPage page.
@@ -14,8 +16,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'modal.html',
 })
 export class ModalPage {
+	//TO DO: cambiar el array a como debe ser
+	 items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+  	/*
+  		Traigo el listado de productos completo y lo guardo en el array
+  	 */
+  	this.http.get('http://codicilabs.com/proyectos/card/index.php/api/getProducts').map(res => res.json()).subscribe(data => {    
+        this.items = data;
+    });
   }
 
   ionViewDidLoad() {
